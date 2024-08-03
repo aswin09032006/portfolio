cf8ef640ce75c5b5be819c1e7f2b6647dd522083
ASWIN V K <vkaswinkanan@gmail.com>
2024-08-03 12:37:54 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,82 @@
-// src/App.js
 import React, { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Preloader from "./components/Preloader";
+import flatline from './images/flatline.mp4';
 import Contact from "./routes/contact/contact";
 import Homepage from "./routes/homepage/homepage";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
-  const location = useLocation();
+  const [isCompatible, setIsCompatible] = useState(true);
 
   useEffect(() => {
-    setLoading(true);
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 1000); // Simulating a 1-second load time for demonstration purposes
+    const isFirstVisit = localStorage.getItem('firstVisit') === null;
 
-    return () => clearTimeout(timer);
-  }, [location]);
+    // Check if the device is compatible (e.g., screen width >= 1024 pixels)
+    const checkCompatibility = () => {
+      const screenWidth = window.innerWidth;
+      if (screenWidth < 1200) {
+        setIsCompatible(false);
+      } else {
+        setIsCompatible(true);
+      }
+    };
+
+    checkCompatibility();
+    window.addEventListener('resize', checkCompatibility);
+
+    return () => window.removeEventListener('resize', checkCompatibility);
+
+  }, []);
+
+  useEffect(() => {
+    const isFirstVisit = localStorage.getItem('firstVisit') === null;
+
+    if (isFirstVisit) {
+      const timer = setTimeout(() => {
+        setLoading(false);
+        localStorage.setItem('firstVisit', 'false');
+      }, 5000);
+
+      return () => clearTimeout(timer);
+    } else {
+      setLoading(false);
+    }
+  }, []);
+
+  if (loading) {
+    return <Preloader />;
+  }
+
+  if (!isCompatible) {
+    return (
+      <div className="not-compatible-container">
+        <video width="100%" height="auto" autoPlay loop muted className="flatlined-video">
+          <source src={flatline} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <p className="not-compatible-text">
+          Please switch to a device with a screen width greater than 1200 pixels for an optimal experience. This feature will be available on smaller screens soon.
+        </p>
+        <a href="https://youtu.be/gvyUuxdRdR4?si=gXiR0vbOAH4HLBlq">
+          <button className="emotional-support-button">
+            Emotional Support
+          </button>
+        </a>
+      </div>
+    );
+  }
 
   return (
-    <div className="App">
-      {loading ? (
-        <Preloader route={location.pathname} />
-      ) : (
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/home" element={<Homepage />} />
-          {/* <Route path="/about" element={<About />} /> */}
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      )}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/home" element={<Homepage />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
-const WrappedApp = () => (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
-
-export default WrappedApp;
+export default App;
